8445aa720080661883cc34c2f3be481cc675e60d
Maria Luiza Rodrigues <marialuiza.r.eng@gmail.com>
2024-02-28 20:01:59 -0300

    feat: adding ListItem style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
+import { v4 as uuidv4 } from "uuid";
+
 import { Header } from "./components/header";
 import { Input } from "./components/input";
 import { ListHeader } from "./components/list/list-header";
 import { ListItem } from "./components/list/list-item";
 
 import styles from "./app.module.css";
+
+const tasks = [
+  {
+    id: uuidv4(),
+    title: "Estudar Next",
+    isComplete: false,
+  },
+  {
+    id: uuidv4(),
+    title: "Estudar Node",
+    isComplete: false,
+  },
+  {
+    id: uuidv4(),
+    title: "Estudar Tailwind",
+    isComplete: false,
+  },
+  {
+    id: uuidv4(),
+    title: "Estudar JavaScript",
+    isComplete: true,
+  },
+  {
+    id: uuidv4(),
+    title: "Estudar React",
+    isComplete: true,
+  },
+];
 
 export function App() {
   return (
@@ -12,7 +42,9 @@ export function App() {
       <Input />
       <div className={styles.listContainer}>
         <ListHeader />
-        <ListItem />
+        {tasks.map((task) => {
+          return <ListItem task={task} key={task.id} />;
+        })}
       </div>
     </>
   );
--- src/components/list/list-item.tsx
+++ src/components/list/list-item.tsx
@@ -1,19 +1,41 @@
 import { Check, Trash } from "@phosphor-icons/react";
 
 import styles from "./list-item.module.css";
-export function ListItem() {
+
+interface TaskType {
+  id: string;
+  title: string;
+  isComplete: boolean;
+}
+
+interface TaskProps {
+  task: TaskType;
+}
+
+export function ListItem({ task }: TaskProps) {
+  const checkboxCheckedClassname = task.isComplete
+    ? styles.checkboxChecked
+    : styles.checkboxUnchecked;
+  const titleCheckedClassname = task.isComplete
+    ? styles.titleCompletedTask
+    : "";
+
   return (
     <div className={styles.listItemContainer}>
-      <Check size={12} className={styles.check} />
-      <p>
-        Integer urna interdum massa libero auctor neque turpis turpis semper.
-        Duis vel sed fames integer.
-      </p>
-      <Trash
-        size={24}
-        color="
+      <label htmlFor="checked">
+        <input readOnly type="checkbox" checked={task.isComplete} />
+        <span className={`${styles.checkbox} ${checkboxCheckedClassname}`}>
+          {task.isComplete && <Check size={10} color="#f2f2f2" />}
+        </span>
+      </label>
+      <p className={`${styles.title} ${titleCheckedClassname}`}>{task.title}</p>
+      <button>
+        <Trash
+          size={16}
+          color="
       #808080"
-      />
+        />
+      </button>
     </div>
   );
 }
