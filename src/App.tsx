59288dcd93d01a0cb566c97d3d14387c9a630066
Maria Luiza Rodrigues <marialuiza.r.eng@gmail.com>
2024-02-28 14:36:47 -0300

    feat: adding listEmpty

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Header } from "./components/header";
 import { Input } from "./components/input";
 import { ListHeader } from "./components/list/list-header";
+import { ListEmpty } from "./components/list/list-empty";
 
 export function App() {
   return (
@@ -9,6 +10,7 @@ export function App() {
       <Input />
       <div>
         <ListHeader />
+        <ListEmpty />
       </div>
     </>
   );
---
+++ src/components/list/list-empty.tsx
@@ -0,0 +1,14 @@
+import Clipboard from "../../assets/clipboard.png";
+
+import styles from "./list-empty.module.css";
+export function ListEmpty() {
+  return (
+    <div className={styles.listEmptyContainer}>
+      <img src={Clipboard} alt="Ícone de prancheta" />
+      <p>
+        <strong>Voce ainda não tem tarefas cadastradas</strong>Crie tarefas e
+        organize seus itens a fazer
+      </p>
+    </div>
+  );
+}
